741fafd85dd86938381a6a827dd736f7c5c055de
Jack Dietrich <jed123@rogers.com>
2025-06-23 13:36:39 -0400

    Re-organised into public folder for static elements. Express now able to serve static page.

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { readFile } = require('fs').promises;
 
 const app = express();// create new express instance
 
+app.use(express.static('public'))//to serve the static part of the page.
 
-app.get('/', async (request,response) => {
+app.get('/index.html', async (request,response) => {
 
-    response.send( await readFile('./index.html','utf8'))
+    response.send( await readFile('/','utf8'))
 })
 
 app.listen(process.env.PORT || 3000, () => console.log("App is running"))
